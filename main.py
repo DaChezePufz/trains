c06bc891dcf6023c903e894e59fb18de3ccc50f5
Jake Warner <jakewarner98@gmail.com>
2022-10-05 19:04:59 +0100

    Update main.py
    Co-Authored-By: CaseyDev <86911050+CaseysDev@users.noreply.github.com>

--- main.py
+++ main.py
@@ -27,18 +27,21 @@ sortedObject = sortObject(sortedRaw[3])
 type, platform = sortedObject[3].split('":"')
 arrtype, arrive = sortedObject[6].split('":"')
 leavtype, leave = sortedObject[7].split('":"')
+desttype, dest = sortedObject[10].split('":"')
 
 # Train 2
 sortedObject1 = sortObject(sortedRaw[5])
 type1, platform1 = sortedObject1[3].split('":"')
 arrtype1, arrive1 = sortedObject1[6].split('":"')
 leavtype1, leave1 = sortedObject1[7].split('":"')
+desttype1, dest1 = sortedObject1[10].split('":"')
 
 # Train 3
 sortedObject2 = sortObject(sortedRaw[7])
 type1, platform2 = sortedObject2[3].split('":"')
 arrtype1, arrive2 = sortedObject2[6].split('":"')
 leavtype1, leave2 = sortedObject2[7].split('":"')
+desttype1, dest2 = sortedObject2[10].split('":"')
 
 print(f"""
 
@@ -47,17 +50,20 @@ Train 1:
 Platform: {removePunc(platform)}
 Arriving at station @ {removePunc(arrive)}
 Leaving station @ {removePunc(leave)}
+Destination: {removePunc(dest)}
 
 Train 2:
 
 Platform: {removePunc(platform1)}
 Arriving at station @ {removePunc(arrive1)}
 Leaving station @ {removePunc(leave1)}
+Destination: {removePunc(dest1)}
 
 Train 3:
 
 Platform: {removePunc(platform2)}
 Arriving at station @ {removePunc(arrive2)}
 Leaving station @ {removePunc(leave2)}
+Destination: {removePunc(dest2)}
 
 """)
