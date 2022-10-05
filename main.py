a870009aeab3e8cff71eb995e76c250088e61b29
Jake Warner <jakewarner98@gmail.com>
2022-10-05 18:55:46 +0100

    Update main.py
    Co-Authored-By: CaseyDev <86911050+CaseysDev@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import requests
 
-response = requests.get("https://transportapi.com/v3/uk/train/station/adv///timetable.json?app_id=41f6042a&app_key=229f789dd8d9c6d488333521d8960127&train_status=passenger")
+response = requests.get("https://transportapi.com/v3/uk/train/station/adv///timetable.json?app_id=30f759a5&app_key=855270cacc33dfe9e04b2de90ee5835f&train_status=passenger")
 rawText = response.text
+
+def removePunc(message):
+    finalised = []
+
+    for char in message:
+        if(char != '"'):
+            finalised.append(char)
+
+    return "".join(finalised)
 
 def sortRaw(message):
     list = message.split("{")
@@ -12,7 +21,43 @@ def sortObject(message):
     return list
 
 sortedRaw = sortRaw(rawText)
-sortedObject = sortObject(sortedRaw[3])
 
-print(sortedObject[7])
-print(sortedObject[6])
+# Train 1
+sortedObject = sortObject(sortedRaw[3])
+type, platform = sortedObject[3].split('":"')
+arrtype, arrive = sortedObject[6].split('":"')
+leavtype, leave = sortedObject[7].split('":"')
+
+# Train 2
+sortedObject1 = sortObject(sortedRaw[5])
+type1, platform1 = sortedObject1[3].split('":"')
+arrtype1, arrive1 = sortedObject1[6].split('":"')
+leavtype1, leave1 = sortedObject1[7].split('":"')
+
+# Train 3
+sortedObject2 = sortObject(sortedRaw[7])
+type1, platform2 = sortedObject2[3].split('":"')
+arrtype1, arrive2 = sortedObject2[6].split('":"')
+leavtype1, leave2 = sortedObject2[7].split('":"')
+
+print(f"""
+
+Train 1:
+
+Platform: {removePunc(platform)}
+Arriving at station @ {removePunc(arrive)}
+Leaving station @ {removePunc(leave)}
+
+Train 2:
+
+Platform: {removePunc(platform1)}
+Arriving at station @ {removePunc(arrive1)}
+Leaving station @ {removePunc(leave1)}
+
+Train 3:
+
+Platform: {removePunc(platform2)}
+Arriving at station @ {removePunc(arrive2)}
+Leaving station @ {removePunc(leave2)}
+
+""")
