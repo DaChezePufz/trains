d50a1344036140381a76e0725083553d0e9f1c68
Jake Warner <jakewarner98@gmail.com>
2022-10-05 16:12:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,18 @@
-from lib2to3.pgen2.token import GREATER
-import apiDetails
 import requests
-import json
-from datetime import datetime
-class MakeApiCall:
 
-    def get_data(self, api):
-        response = requests.get(f"{api}")
-        if response.status_code == 200:
-            print("sucessfully fetched the data")
-            self.formatted_print(response.json())
-        else:
-            print(f"Hello person, there's a {response.status_code} error with your request")
+response = requests.get("https://transportapi.com/v3/uk/train/station/adv///timetable.json?app_id=41f6042a&app_key=229f789dd8d9c6d488333521d8960127&train_status=passenger")
+rawText = response.text
 
-    def get_user_data(self, api, parameters):
-        response = requests.get(f"{api}", params=parameters)
-        if response.status_code == 200:
-            print("sucessfully fetched the data with parameters provided")
-            self.formatted_print(response.json())
-        else:
-            print(
-                f"Hello person, there's a {response.status_code} error with your request")
+def sortRaw(message):
+    list = message.split("{")
+    return list
 
-    def formatted_print(self, obj):
-        text = json.dumps(obj, sort_keys=True, indent=4)
-        print(text)
+def sortObject(message):
+    list = message.split(",")
+    return list
 
-    def __init__(self, api):
-        # self.get_data(api)
+sortedRaw = sortRaw(rawText)
+sortedObject = sortObject(sortedRaw[3])
 
-        parameters = {
-#            "username": ""
-            
-        }
-        self.get_user_data(api, parameters)
-
-if __name__ == "__main__":
-    __init__()
-
-station_code = "grt"
-date = ""
-time = ""
-
-
-api_call = MakeApiCall("https://transportapi.com/v3/uk/train/station/bsk///timetable.json?app_id=b1f965c3&app_key=7d35c36842fcda2da625d900b2c53326&train_status=passsenger")
-
+print(sortedObject[7])
+print(sortedObject[6])
