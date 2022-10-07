7967d8144e058e23678381bd31532f9e1a1a1b33
Jake Warner <jakewarner98@gmail.com>
2022-10-07 17:02:53 +0100

    creation of grt.py
    grt.py adds manual platforms to the station grt which doesn't report any platforms with the transport api
    
    Co-Authored-By: CaseyDev <86911050+caseysdev@users.noreply.github.com>

---
+++ grt.py
@@ -0,0 +1,13 @@
+plat1 = {
+    "London Waterloo",
+    "Basingstoke",
+    "Reading"
+}
+
+plat2 = {
+    "Yeovil Junction",
+    "Frome",
+    "Salisbury",
+    "Exeter St Davids",
+    "Crewkerne"
+}
