f49fd2d6a843996ab307d16d3bb85bee2eac43e6
Jake Warner <jakewarner98@gmail.com>
2022-10-12 14:51:51 +0100

    Update main.py
    Updating main.py to now use functions.py as it cleans up the look of the code
    
    Co-Authored-By: CaseyDev <86911050+CaseysDev@users.noreply.github.com>

--- python/main.py
+++ python/main.py
@@ -5,77 +5,33 @@ import os           # importing 'os'
 import grt          # importing 'grt.py'        [LOCAL]
 import stations     # importing 'stations.py'   [LOCAL]
 import datetime     # importing 'datettime'
-import afterDateNds # importing 'afterDateNds   [LOCAL]
 import math         # importing 'math'
+import functions    # importing 'functions.py'  [LOCAL]
 
 ## clearing the shell output
 os.system("cls")
 
-## converting the foreighn dictionary to local
-prefixDateList = afterDateNds.afterDatePrefix
 
-## getting the date and time at which the query was placed
-now = datetime.datetime.now()
-
-## getting all the time values as their own seperate variables
-currentWeekDay = now.strftime("%A")
-currentMonth = now.strftime("%B")
-currentYear = now.strftime("%Y")
-currentMonthDay = now.strftime("%d")
-
-for dayOfMonth, dayOfMonthSuffix in prefixDateList.items():
-    if dayOfMonth == currentMonthDay:
-        monthDayPrefix = dayOfMonthSuffix
-
-currentHour = now.strftime("%H")
-currentMinute = now.strftime("%M")
-currentSecond = now.strftime("%S")
-
-print(currentWeekDay,currentMonthDay+monthDayPrefix,currentMonth,currentYear+" - "+currentHour+":"+currentMinute+":"+currentSecond)
 
 ## defining the database as a local variable
-stationsDB = stations.stationsInDict
+
 
 platform = 0
 numSpacesSixthRow = 0
 
-## checking to see that the user has api credentails present
-if apiKeys.apiKey == "":
-    print("Please enter an API Key in the \'apiKeys.py\' file.")
-    exit()
-if apiKeys.appID == "":
-    print("Please enter an app ID in the \'apiKeys.py\' file.")
-    exit()
 
-## checking if default station preset in 'apiKeys.py', if not requesting one from user
-if apiKeys.station == "":
-	station = input("Enter station code:  ")
-else: 
-	station = apiKeys.station
+
+
 
 ## making 2nd station code var with uppercase
-stationUpper = station.upper()
 
-## searching for and displaying the full station code
-for stationName, stationCode in stationsDB.items():
-    if stationUpper == stationCode:
-        print(f"The station is: {stationName}")
-        stationChosen = stationName
 
-## fixing certain stations that don't have platforms assined to train manually
-def grtPlatWorkings(destinationWPunc):
-    destination = removePunc(destinationWPunc)
-    if station == "grt":
-        if destination in grt.plat1:
-            platform = "1"
-        elif destination in grt.plat2:
-            platform = "2"
-        else:
-            platform = "N/A"
-        return platform
+
+
+
 
 ## making the url for the api call
-url1 = "https://transportapi.com/v3/uk/train/station/"+station.lower()+"///timetable.json?app_id="+apiKeys.appID+"&app_key="+apiKeys.apiKey+"&train_status=passenger"
+url1 = "https://transportapi.com/v3/uk/train/station/"+functions.station.lower()+"///timetable.json?app_id="+apiKeys.appID+"&app_key="+apiKeys.apiKey+"&train_status=passenger"
 
 ## defining response for for the request statment, otherwise errors
 response = ""
@@ -84,24 +40,9 @@ try: response = requests.get(url1)
 except: print("Error - check your network connection")
 rawText = response.text
 
-## some formatting and json parsing
-def removePunc(message):
-    finalised = []
 
-    for char in message:
-        if(char != '"'):
-            finalised.append(char)
-    return "".join(finalised)
 
-def sortRaw(message):
-    list = message.split("{")
-    return list
-
-def sortObject(message):
-    list = message.split(",")
-    return list
-
-sortedRaw = sortRaw(rawText)
+sortedRaw = functions.sortRaw(rawText)
 
 print("""
             Modes:
@@ -126,12 +67,12 @@ trainDictValue = [3,5,7,9,11]
 ## if the train starts from that location (and other similar situations)
 try: 
     for i in range(1, (numOfTrainsToDisplay+1)):
-        sortedObject = sortObject(sortedRaw[trainDictValue[i]])
+        sortedObject = functions.sortObject(sortedRaw[trainDictValue[i]])
 
         try: desttype, dest = sortedObject[10].split('":"')
         except: dest = "N/A"
         try: type, platform = sortedObject[3].split('":"')
-        except: platform = grtPlatWorkings(dest)
+        except: platform = functions.grtPlatWorkings(dest)
         try: arrtype, arrive = sortedObject[7].split('":"')
         except: arrive = "N/A"
         try: leavtype, leave = sortedObject[6].split('":"')
@@ -145,10 +86,10 @@ def commandLineDisplay():
         print(f"""
 
 Train {i}:
-Platform: {removePunc(platform)}
-Arriving at station @ {removePunc(arrive)}
-Leaving station @ {removePunc(leave)}
-Destination: {removePunc(dest)}
+Platform: {functions.removePunc(platform)}
+Arriving at station @ {functions.removePunc(arrive)}
+Leaving station @ {functions.removePunc(leave)}
+Destination: {functions.removePunc(dest)}
         """)
     except: print("")
     os.system("pause")
@@ -161,7 +102,7 @@ def displayBoardsDisplay():
     print("")
     # 40 wide, 1 space either side for padding then box characters
     if numOfTrainsToDisplay == 1:
-        lengthStationName = len(stationChosen)
+        lengthStationName = len(functions.stationChosen)
         numOfSpacesToPrint = 40 - lengthStationName
 
         if numOfSpacesToPrint >= 1:
@@ -182,18 +123,18 @@ def displayBoardsDisplay():
                 frontSpaces = " " * int(frontNumSpaces)
                 backSpaces = " " * int(backNumSpaces)
 
-            stationNameToDisplay = frontSpaces+stationChosen+backSpaces
+            stationNameToDisplay = frontSpaces+functions.stationChosen+backSpaces
         else:
-            cutStationName = stationChosen[:40]
+            cutStationName = functions.stationChosen[:40]
             stationNameToDisplay = cutStationName
-        try: platformWithoutPunc = removePunc(platform)
+        try: platformWithoutPunc = functions.removePunc(platform)
         except: platformWithoutPunc = platform
         if int(platformWithoutPunc) >= 40:
             spaceIfPlat2Chars = ""
         else:
             spaceIfPlat2Chars = " "
 
-        lengthDestination = len(removePunc(dest))
+        lengthDestination = len(functions.removePunc(dest))
 
         numSpacesSixthRow = 40 - (lengthDestination + 5 + 2)
         numSpacesSixthRowPrint = " " * int(numSpacesSixthRow)
@@ -202,9 +143,9 @@ def displayBoardsDisplay():
 ┌──────────────────────────────────────────┐
 │ {stationNameToDisplay} │
 │                                          │
-│                                  {spaceIfPlat2Chars}Plat {removePunc(platform)} │
+│                                  {spaceIfPlat2Chars}Plat {functions.removePunc(platform)} │
 │                                          │
-│ {removePunc(dest)} {numSpacesSixthRowPrint} {removePunc(leave)} │
+│ {functions.removePunc(dest)} {numSpacesSixthRowPrint} {functions.removePunc(leave)} │
 │                                          │
 │                                          │
 │                                          │
